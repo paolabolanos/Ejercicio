2a6ce3e611da098e34abc57598438eb43aaa7d10
Paola Bolaños <paola.bolanos@outlook.es>
2024-07-11 12:08:53 -0600

    Repositorio

---
+++ src/ejerciciodegit/EjercicioDeGit.java
@@ -0,0 +1,20 @@
+
+package ejerciciodegit;
+
+import java.util.Scanner;
+
+
+public class EjercicioDeGit {
+
+    
+    public static void main(String[] args) { 
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Tabla de multiplicar del 10"); 
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(10 + "*" + i + "=" +10*i);  
+        }
+            
+        }
+    }
+    
+
